94fc0178408fbb1faef40c441cfc2589609f5a9a
Wagner Franzé Junior <franzejunior2012@hotmail.com>
2018-09-01 23:56:10 -0300

    Commit initial

---
+++ Test.php
@@ -0,0 +1,23 @@
+<?php
+class Test{
+	
+	private $name;
+	private $type;
+	
+	public function setName($name){
+		$this->name = $name;
+	}
+	
+	public function getName(){
+		return $this->name;
+	}
+	
+	public function setType($type){
+		$this->type = $type;
+	}
+	
+	public function getType(){
+		return $this->type;
+	}
+	
+}
